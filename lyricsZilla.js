5690688907bc91751f03556d9fea119062dc8d02
Rui Esteves <ruicarest@gmail.com>
2019-06-16 10:55:29 +0100

    testing

---
+++ lyricsZilla.js
@@ -0,0 +1,14 @@
+var unirest = require('unirest');
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Helloas World!');
+}).listen(8080);
+
+unirest.get("https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist=michael%20jackson&song=bad")
+.header("X-RapidAPI-Host", "sridurgayadav-chart-lyrics-v1.p.rapidapi.com")
+.header("X-RapidAPI-Key", "0305af4769msh0e1832ecd3a9cf6p12d2fdjsn350aa46d4a9f")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
